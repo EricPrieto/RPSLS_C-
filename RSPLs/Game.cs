8395b6fe6f778115549fbd002fad8f6978595dab
EricPrieto <ep1043@hotmail.com>
2021-11-16 16:59:39 -0600

    Created Player file with gesture list

---
+++ RSPLs/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSPLs
+{
+    public class Game
+    {
+
+        public void Run ()
+        {
+            Welcome();
+
+        }
+
+        public void Welcome()
+        {
+            Console.WriteLine("WELCOME TO RPSLS!");
+        }
+
+
+    }
+}
+
---
+++ RSPLs/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSPLs
+{
+    public class Player
+    {
+        //member variable
+        public string name;
+        public List<string> gestures;
+        int win =0;
+        int lose = 0;
+        int tie =0;
+
+        //member methods
+        public Player()
+        {
+            gestures = new List<string>();
+            CreateGesture();
+        }
+
+         public void CreateGesture()
+        {
+            gestures.Add("rock");
+            gestures.Add("paper");
+            gestures.Add("scissors");
+            gestures.Add("lizard");
+            gestures.Add("spock");
+        }
+    }
+}
